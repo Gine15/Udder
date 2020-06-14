d12409f1554d5c231abdea331c72dde0098ec7e0
Leon Tay <leon.kokyong@gmail.com>
2020-06-14 15:24:44 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Leon Tay on 14/6/20.
+//  Copyright © 2020 Leon Tay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
